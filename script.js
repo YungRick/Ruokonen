cdd4118573538dc0ef5c8bfcddd69bee1c89fdc0
YungRick <riku.ruokonen@outlook.com>
2017-01-29 18:30:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,5 +5,20 @@ $(document).ready(function(){
   $('.enterbutton').mouseleave(function(){
 	  $(this).fadeTo('slow', 1);	
   });
+	
+	/*var lastScrollTop = 0;
+	$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st > lastScrollTop){
+       $(".backbutton").fadeOut("slow");
+			 $(".backbutton").hide();
+   } else {
+		 $(".backbutton").hide();
+      $(".backbutton").fadeIn("slow");
+   }
+   lastScrollTop = st;
+});
+
+*/
    
 });  
